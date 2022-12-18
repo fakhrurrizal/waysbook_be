34c876369336b27cc3e59291116d62365bc95e25
Muhammad Fakhrur Rizal <fakhrurrizal10@gmail.com>
2022-12-18 22:31:21 +0700

    deploy be

---
+++ routes/transaction.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"waysbuck/handlers"
+	"waysbuck/pkg/middleware"
+	"waysbuck/pkg/mysql"
+	"waysbuck/repositories"
+
+	"github.com/gorilla/mux"
+)
+
+func TransactionRoutes(r *mux.Router) {
+	transactionRepository := repositories.RepositoryTransaction(mysql.DB)
+	h := handlers.HandlerTransaction(transactionRepository)
+
+	r.HandleFunc("/transactions", middleware.Auth(h.FindTransaction)).Methods("GET")
+	r.HandleFunc("/transaction/{id}", h.GetTransaction).Methods("GET")
+	r.HandleFunc("/transaction", middleware.Auth(h.CreateTransaction)).Methods("POST")
+	r.HandleFunc("/transaction", middleware.Auth(h.UpdateTransaction)).Methods("PATCH")
+	r.HandleFunc("/transaction-user", middleware.Auth(h.GetTransactionByUser)).Methods("GET")
+	
+	// r.HandleFunc("/transaction/{id}", middleware.Auth(h.DeleteTransaction)).Methods("DELETE")
+	r.HandleFunc("/my-order", middleware.Auth(h.GetOrderByID)).Methods("GET")
+	r.HandleFunc("/transUpdate/{id}", middleware.Auth(h.UpdateAdmin)).Methods("PATCH")
+	r.HandleFunc("/notification", h.Notification).Methods("POST")
+}
